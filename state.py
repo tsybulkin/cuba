2d5aee3ad79c968859e8b275b1190929c6dd074d
Ian Tsybulkin <ian.tsybulkin@gmail.com>
2017-03-10 09:19:29 +0200

    learning added

--- state.py
+++ state.py
@@ -2,6 +2,9 @@
 # this module deals with state of the robot
 #
 
+GAMA = 0.95
+LRATE = 0.3
+
 
 
 def x_to_state(x):
@@ -15,5 +18,28 @@ def get_legal_actions(state):
 	return [-2, -1, 0 , 1, 2]
 
 
+def get_policy(state, Qtab, eps=0.):
+	if flip_coin(eps):
+		return get_random_policy(state)
 
-	
+	actions = get_legal_actions(state)
+	i = np.argmax( Qtab.get((state,a), 0.) for a in actions)
+	return actions[i]
+
+
+
+def get_random_policy(state):
+	return np.random.choice(get_legal_actions(state))
+
+
+
+def learn(state, action, state1, Qtab):
+	V = Qtab.get((state, action), 0.)
+	V1 = max( Qtab.get((state1,a)) for a in get_legal_actions(state1) )
+	R = reward(state, action, state1)
+	Qtab[(state, action)] = (1 - LRATE) * V + LRATE * (R + GAMA * V1)
+
+
+
+
+
--- train.py
+++ train.py
@@ -1,6 +1,17 @@
 # this module provides training functions
 #
+from state import init_state
 
+
+TAU = 0.001
 
 
 def train_epoch(T, Qtab):
+	""" x = [a, a1, w]
+	"""
+	t = 0.
+	x = np.array([np.pi, 0., 0.])
+
+	while t < T:
+
+		t += TAU
